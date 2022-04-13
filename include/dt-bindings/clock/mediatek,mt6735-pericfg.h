3f0a5630cfd30bbcc04f6ace45d76946bc8fab00
Yassine Oudjana <y.oudjana@protonmail.com>
2022-04-13 07:55:41 +0400

    dt-bindings: clock: Add Mediatek MT6735 clock bindings
    Add clock definitions for Mediatek MT6735 clocks provided by
    apmixedsys, topckgen, infracfg and pericfg.
    
    Signed-off-by: Yassine Oudjana <y.oudjana@protonmail.com>
    Acked-by: Rob Herring <robh@kernel.org>

---
+++ include/dt-bindings/clock/mediatek,mt6735-pericfg.h
@@ -0,0 +1,37 @@
+/* SPDX-License-Identifier: (GPL-2.0-only OR BSD-2-Clause) */
+
+#ifndef _DT_BINDINGS_CLK_MT6735_PERICFG_H
+#define _DT_BINDINGS_CLK_MT6735_PERICFG_H
+
+#define DISP_PWM			0
+#define THERM				1
+#define PWM1				2
+#define PWM2				3
+#define PWM3				4
+#define PWM4				5
+#define PWM5				6
+#define PWM6				7
+#define PWM7				8
+#define PWM				9
+#define USB0				10
+#define IRDA				11
+#define APDMA				12
+#define MSDC30_0			13
+#define MSDC30_1			14
+#define MSDC30_2			15
+#define MSDC30_3			16
+#define UART0				17
+#define UART1				18
+#define UART2				19
+#define UART3				20
+#define UART4				21
+#define BTIF				22
+#define I2C0				23
+#define I2C1				24
+#define I2C2				25
+#define I2C3				26
+#define AUXADC				27
+#define SPI0				28
+#define IRTX				29
+
+#endif
