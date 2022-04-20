6cf19ca332c927e56db6dba7be4b9170ffb37f76
Yassine Oudjana <y.oudjana@protonmail.com>
2022-04-20 12:37:12 +0400

    dt-bindings: reset: Add MediaTek MT6735 MMSYS bindings
    Add definitions for MT6735 multimedia system resets.
    
    Signed-off-by: Yassine Oudjana <y.oudjana@protonmail.com>

---
+++ include/dt-bindings/reset/mediatek,mt6735-mmsys.h
@@ -0,0 +1,34 @@
+/* SPDX-License-Identifier: (GPL-2.0-only OR BSD-2-Clause) */
+
+#ifndef _DT_BINDINGS_RESET_MT6735_MMSYS_H
+#define _DT_BINDINGS_RESET_MT6735_MMSYS_H
+
+#define RST_SMI_COMMON			0
+#define RST_SMI_LARB0			1
+#define RST_CAM_MDP			2
+#define RST_MDP_RDMA			3
+#define RST_MDP_RSZ0			4
+#define RST_MDP_RSZ1			5
+#define RST_MDP_TDSHP			6
+#define RST_MDP_WDMA			7
+#define RST_MDP_WROT			8
+#define RST_FAKE_ENG			9
+#define RST_MUTEX			10
+#define RST_DISP_OVL0			11
+#define RST_DISP_RDMA0			12
+#define RST_DISP_RDMA1			13
+#define RST_DISP_WDMA0			14
+#define RST_DISP_COLOR			15
+#define RST_DISP_CCORR			16
+#define RST_DISP_AAL			17
+#define RST_DISP_GAMMA			18
+#define RST_DISP_DITHER			19
+#define RST_DISP_UFOE			20
+#define RST_DISP_PWM_MM			21
+#define RST_DSI				22
+#define RST_DPI				23
+#define RST_MIPI_TX_CONFIG		26
+
+#define RST_LCM				32
+
+#endif
