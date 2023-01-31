aaa789023359ebaaff80c735438b1e92d47db3af
Yassine Oudjana <y.oudjana@protonmail.com>
2023-01-31 09:26:53 +0400

    dt-bindings: clock: Add MediaTek MT6735 IMGSYS bindings
    Add definitions for MT6735 imaging system clocks.
    
    Signed-off-by: Yassine Oudjana <y.oudjana@protonmail.com>

---
+++ include/dt-bindings/clock/mediatek,mt6735-imgsys.h
@@ -0,0 +1,15 @@
+/* SPDX-License-Identifier: (GPL-2.0-only OR BSD-2-Clause) */
+
+#ifndef _DT_BINDINGS_CLK_MT6735_IMGSYS_H
+#define _DT_BINDINGS_CLK_MT6735_IMGSYS_H
+
+#define CLK_SMI_LARB2			0
+#define CLK_CAM_SMI			1
+#define CLK_CAM_CAM			2
+#define CLK_SEN_TG			3
+#define CLK_SEN_CAM			4
+#define CLK_CAM_SV			5
+#define CLK_SUFOD			6
+#define CLK_FD				7
+
+#endif
