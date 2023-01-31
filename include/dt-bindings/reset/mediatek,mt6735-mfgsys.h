5a9a28ca4e69eb09d4230f2a571f17091a1ec071
Yassine Oudjana <y.oudjana@protonmail.com>
2023-01-31 10:20:20 +0400

    dt-bindings: reset: Add MediaTek MT6735 MFGSYS bindings
    Add definitions for MT6735 MFlexGraphics system resets.
    
    Signed-off-by: Yassine Oudjana <y.oudjana@protonmail.com>

---
+++ include/dt-bindings/reset/mediatek,mt6735-mfgsys.h
@@ -0,0 +1,9 @@
+/* SPDX-License-Identifier: (GPL-2.0-only OR BSD-2-Clause) */
+
+#ifndef _DT_BINDINGS_RESET_MT6735_MFGSYS_H
+#define _DT_BINDINGS_RESET_MT6735_MFGSYS_H
+
+#define RST_AXI				0
+#define RST_G3D				1
+
+#endif
