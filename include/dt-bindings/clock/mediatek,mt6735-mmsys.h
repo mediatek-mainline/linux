8fb5f3c8778fc460c58099a98182b9db1ad5f575
Yassine Oudjana <y.oudjana@protonmail.com>
2023-01-31 09:03:10 +0400

    dt-bindings: clock: Add MediaTek MT6735 MMSYS bindings
    Add definitions for MT6735 multimedia system clocks.
    
    Signed-off-by: Yassine Oudjana <y.oudjana@protonmail.com>

---
+++ include/dt-bindings/clock/mediatek,mt6735-mmsys.h
@@ -0,0 +1,31 @@
+/* SPDX-License-Identifier: (GPL-2.0-only OR BSD-2-Clause) */
+
+#ifndef _DT_BINDINGS_CLK_MT6735_MMSYS_H
+#define _DT_BINDINGS_CLK_MT6735_MMSYS_H
+
+#define CLK_SMI_COMMON			0
+#define CLK_SMI_LARB0			1
+#define CLK_CAM_MDP			2
+#define CLK_MDP_RDMA			3
+#define CLK_MDP_RSZ0			4
+#define CLK_MDP_RSZ1			5
+#define CLK_MDP_TDSHP			6
+#define CLK_MDP_WDMA			7
+#define CLK_MDP_WROT			8
+#define CLK_FAKE_ENG			9
+#define CLK_DISP_OVL0			10
+#define CLK_DISP_RDMA0			11
+#define CLK_DISP_RDMA1			12
+#define CLK_DISP_WDMA0			13
+#define CLK_DISP_COLOR			14
+#define CLK_DISP_CCORR			15
+#define CLK_DISP_AAL			16
+#define CLK_DISP_GAMMA			17
+#define CLK_DISP_DITHER			18
+#define CLK_AXI_ASIF			19
+#define CLK_DSI_ENGINE			20
+#define CLK_DSI_DIGITAL			21
+#define CLK_DPI_ENGINE			22
+#define CLK_DPI_PIXEL			23
+
+#endif
