d525c429ca60a46faefdbfd3e6719e8d98aa2637
Yassine Oudjana <y.oudjana@protonmail.com>
2023-01-31 09:38:51 +0400

    dt-bindings: reset: Add MediaTek MT6735 VDECSYS bindings
    Add definitions for MT6735 video decoding system resets.
    
    Signed-off-by: Yassine Oudjana <y.oudjana@protonmail.com>

---
+++ include/dt-bindings/reset/mediatek,mt6735-vdecsys.h
@@ -0,0 +1,10 @@
+/* SPDX-License-Identifier: (GPL-2.0-only OR BSD-2-Clause) */
+
+#ifndef _DT_BINDINGS_RESET_MT6735_VDECSYS_H
+#define _DT_BINDINGS_RESET_MT6735_VDECSYS_H
+
+#define RST_VDEC			0
+
+#define RST_SMI_LARB1			32
+
+#endif
