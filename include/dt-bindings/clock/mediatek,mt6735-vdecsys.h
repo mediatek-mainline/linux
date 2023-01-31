8068627e698b076b038ee069882bf9280fb2a53b
Yassine Oudjana <y.oudjana@protonmail.com>
2023-01-31 09:37:50 +0400

    dt-bindings: clock: Add MediaTek MT6735 VDECSYS bindings
    Add definitions for MT6735 video decoding system clocks.
    
    Signed-off-by: Yassine Oudjana <y.oudjana@protonmail.com>

---
+++ include/dt-bindings/clock/mediatek,mt6735-vdecsys.h
@@ -0,0 +1,9 @@
+/* SPDX-License-Identifier: (GPL-2.0-only OR BSD-2-Clause) */
+
+#ifndef _DT_BINDINGS_CLK_MT6735_VDECSYS_H
+#define _DT_BINDINGS_CLK_MT6735_VDECSYS_H
+
+#define CLK_VDEC			0
+#define CLK_SMI_LARB1			1
+
+#endif
